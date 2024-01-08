d7227263fc059d7a9d6b054bd2493cd0995dff86
asterich <320873791@qq.com>
2024-01-08 23:35:17 +0800

    feat: refactor LayerInfer, now only attention kernel in it

--- model/layers/layer_infer_lightllm/transformer_layer_infer.py
+++ model/layers/layer_infer_lightllm/transformer_layer_infer.py
@@ -15,10 +15,10 @@ class BaseLayerInfer:
     def __init__(self) -> None:
         pass
 
-    def context_forward(self, input_ids, infer_state: InferStateInfo, layer_weight: BaseLayerWeight):
+    def context_forward(self, input_ids, infer_state: InferStateInfo):
         raise Exception("need to impl")
 
-    def token_forward(self, input_ids, infer_state: InferStateInfo, layer_weight: BaseLayerWeight):
+    def token_forward(self, input_ids, infer_state: InferStateInfo):
         raise Exception("need to impl")
     
 
@@ -48,13 +48,7 @@ class TransformerLayerInferTpl(TransformerLayerInfer):
         self.embed_dim_ = -1
         return
     
-    def _att_norm(self, input, infer_state:InferStateInfo, layer_weight)->torch.Tensor:
-        raise Exception("need to impl")
-    
-    def _ffn_norm(self, input, infer_state:InferStateInfo, layer_weight)->torch.Tensor:
-        raise Exception("need to impl")
-    
-    def _pre_cache_kv(self, infer_state:InferStateInfo, layer_weight)->Tuple[torch.Tensor, torch.Tensor]:
+    def _pre_cache_kv(self, infer_state:InferStateInfo)->Tuple[torch.Tensor, torch.Tensor]:
         if infer_state.mem_is_contiguous:
             cache_k = infer_state.mem_manager.key_buffer[self.layer_num_][infer_state.mem_start:infer_state.mem_end, :, :]
             cache_v = infer_state.mem_manager.value_buffer[self.layer_num_][infer_state.mem_start:infer_state.mem_end, :, :]
@@ -62,11 +56,16 @@ class TransformerLayerInferTpl(TransformerLayerInfer):
             cache_k = infer_state.key_buffer
             cache_v = infer_state.value_buffer 
         return cache_k, cache_v
-
-    def _get_qkv(self, input, cache_k, cache_v, infer_state:InferStateInfo, layer_weight)->Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        raise Exception("need to impl")
     
-    def _post_cache_kv(self, cache_k, cache_v, infer_state:InferStateInfo, layer_weight):
+    def _copy_kv_to_kvcache(self, k, v, cache_k, cache_v):
+        '''
+        copy k, v we generated in "qkv_proj" to cache_k, cache_v
+        '''
+        cache_k.view(-1, self.tp_k_head_num_ * self.head_dim_).copy_(k.view(-1, self.tp_k_head_num_ * self.head_dim_))
+        cache_v.view(-1, self.tp_v_head_num_ * self.head_dim_).copy_(v.view(-1, self.tp_v_head_num_ * self.head_dim_))
+        return
+    
+    def _post_cache_kv(self, cache_k, cache_v, infer_state:InferStateInfo):
         mem_manager = infer_state.mem_manager
         if not infer_state.mem_is_contiguous:
             self._copy_kv_to_mem_cache(cache_k, cache_v, infer_state.mem_index, mem_manager)
@@ -77,78 +76,34 @@ class TransformerLayerInferTpl(TransformerLayerInfer):
         destindex_copy_kv(value_buffer, mem_index, mem_manager.value_buffer[self.layer_num_])
         return
     
-    def _context_attention_kernel(self, q, k, v, infer_state:InferStateInfo, layer_weight, out=None)->torch.Tensor:
+    def _context_attention_kernel(self, q, k, v, infer_state:InferStateInfo, out=None)->torch.Tensor:
         raise Exception("need to impl")
     
-    def _token_attention_kernel(self, q, infer_state:InferStateInfo, layer_weight, out=None)->torch.Tensor:
-        raise Exception("need to impl")
-
-    def _get_o(self, input, infer_state:InferStateInfo, layer_weight)->torch.Tensor:
-        raise Exception("need to impl")
-
-    def _ffn(self, input, infer_state:InferStateInfo, layer_weight)->torch.Tensor:
+    def _token_attention_kernel(self, q, infer_state:InferStateInfo, out=None)->torch.Tensor:
         raise Exception("need to impl")
 
 
-    def _context_attention(self, input_embding, infer_state: InferStateInfo, layer_weight):
-        input1 = self._att_norm(input_embding, infer_state, layer_weight)
-        cache_k, cache_v = self._pre_cache_kv(infer_state, layer_weight)
-        q, cache_k, cache_v  = self._get_qkv(input1, cache_k, cache_v, infer_state, layer_weight)
-        input1 = None
-        self._post_cache_kv(cache_k, cache_v, infer_state, layer_weight)
-        o = self._context_attention_kernel(q, cache_k, cache_v, infer_state, layer_weight)
-        q = None
-        o = self._get_o(o, infer_state, layer_weight)
-        if self.world_size_ > 1:
-            dist.all_reduce(o, op=dist.ReduceOp.SUM, async_op=False)
-        input_embding.add_(o.view(-1, self.embed_dim_))
-        return
+    def _context_attention(self, q, k, v, infer_state: InferStateInfo):
+        cache_k, cache_v = self._pre_cache_kv(infer_state)
+        self._copy_kv_to_kvcache(k, v, cache_k, cache_v)
+        self._post_cache_kv(cache_k, cache_v, infer_state)
+        o = self._context_attention_kernel(q, cache_k, cache_v, infer_state)
+        return o
 
-    def _context_ffn(self, input_embdings, infer_state: InferStateInfo, layer_weight):
-        input1 = self._ffn_norm(input_embdings, infer_state, layer_weight)
-        ffn_out = self._ffn(input1, infer_state, layer_weight)
-        input1 = None
-        if self.world_size_ > 1:
-            dist.all_reduce(ffn_out, op=dist.ReduceOp.SUM, async_op=False)
-        input_embdings.add_(ffn_out.view(-1, self.embed_dim_))
-        return
-
-    def _token_attention(self, input_embding, infer_state: InferStateInfo, layer_weight):
-        input1 = self._att_norm(input_embding, infer_state, layer_weight)
-        cache_k, cache_v = self._pre_cache_kv(infer_state, layer_weight)
-        q, cache_k, cache_v = self._get_qkv(input1, cache_k, cache_v, infer_state, layer_weight)
-        input1 = None
-        self._post_cache_kv(cache_k, cache_v, infer_state, layer_weight)
-        o = self._token_attention_kernel(q, infer_state, layer_weight)
-        q = None
-        o = self._get_o(o, infer_state, layer_weight)
-        if self.world_size_ > 1:
-            dist.all_reduce(o, op=dist.ReduceOp.SUM, async_op=False)
-        input_embding.add_(o.view(-1, self.embed_dim_))
-        return
-
-    def _token_ffn(self, input_embdings, infer_state: InferStateInfo, layer_weight):
-        input1 = self._ffn_norm(input_embdings, infer_state, layer_weight)
-        ffn_out = self._ffn(input1, infer_state, layer_weight)
-        input1 = None
-        if self.world_size_ > 1:
-            dist.all_reduce(ffn_out, op=dist.ReduceOp.SUM, async_op=False)
-        input_embdings.add_(ffn_out.view(-1, self.embed_dim_))
-        return
+    def _token_attention(self, q, k, v, infer_state: InferStateInfo):
+        cache_k, cache_v = self._pre_cache_kv(infer_state)
+        self._copy_kv_to_kvcache(k, v, cache_k, cache_v)
+        self._post_cache_kv(cache_k, cache_v, infer_state)
+        o = self._token_attention_kernel(q, infer_state)
+        return o
     
-    def context_forward(self, input_embdings, infer_state: InferStateInfo, layer_weight):
-        self._context_attention(input_embdings,
-                                      infer_state,
-                                      layer_weight=layer_weight)
-        self._context_ffn(input_embdings, infer_state, layer_weight)
-        return input_embdings
+    def context_forward(self, q, k, v, infer_state: InferStateInfo):
+        self._context_attention(q, k, v, infer_state)
+        return None
 
-    def token_forward(self, input_embdings, infer_state: InferStateInfo, layer_weight):
-        self._token_attention(input_embdings,
-                                    infer_state,
-                                    layer_weight=layer_weight)
-        self._token_ffn(input_embdings, infer_state, layer_weight)
-        return input_embdings
+    def token_forward(self, q, k, v, infer_state: InferStateInfo):
+        self._token_attention(q, k, v, infer_state)
+        return None
     
 
 
@@ -171,13 +126,7 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         return
     
     def _bind_func(self):
-        self._bind_norm()
         self._bind_attention()
-        return
-    
-    def _bind_norm(self):
-        self._att_norm = partial(LlamaTransformerLayerInfer._att_norm, self)
-        self._ffn_norm = partial(LlamaTransformerLayerInfer._ffn_norm, self)
         return
     
     def _bind_attention(self):
@@ -185,18 +134,8 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         self._token_attention_kernel = partial(LlamaTransformerLayerInfer._token_decode_attention_gqa_flashdecoding, self)
         self._copy_kv_to_mem_cache = partial(LlamaTransformerLayerInfer._copy_kv_to_mem_cache_normal, self)
         
-
-    def _get_qkv(self, input, cache_k, cache_v, infer_state:LlamaInferStateInfo, layer_weight:LlamaTransformerLayerWeight)->torch.Tensor:
-        q = torch.mm(input.view(-1, self.embed_dim_), layer_weight.q_weight_)
-        rotary_emb_fwd(q.view(-1, self.tp_q_head_num_, self.head_dim_), infer_state.position_cos, infer_state.position_sin)
-        torch.mm(input.view(-1, self.embed_dim_), layer_weight.k_weight_,
-                    out=cache_k.view(-1, self.tp_k_head_num_ * self.head_dim_))
-        rotary_emb_fwd(cache_k, infer_state.position_cos, infer_state.position_sin)
-        torch.mm(input.view(-1, self.embed_dim_), layer_weight.v_weight_,
-                    out=cache_v.view(-1, self.tp_v_head_num_ * self.head_dim_))
-        return q, cache_k, cache_v
     
-    def _context_attention_kernel(self, q, k, v, infer_state:LlamaInferStateInfo, layer_weight, out=None)->torch.Tensor:
+    def _context_attention_kernel(self, q, k, v, infer_state:LlamaInferStateInfo, out=None)->torch.Tensor:
         o_tensor = torch.empty_like(q) if out is None else out
         context_attention_fwd(q.view(-1, self.tp_q_head_num_, self.head_dim_),
                               k.view(-1, self.tp_k_head_num_, self.head_dim_),
@@ -208,28 +147,13 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         return o_tensor
     
     
-    def _get_o(self, input, infer_state:LlamaInferStateInfo, layer_weight:LlamaTransformerLayerWeight)->torch.Tensor:
-        o_tensor = torch.mm(input.view(-1, self.tp_o_head_num_ * self.head_dim_), layer_weight.o_weight_)
-        return o_tensor
-
-    def _ffn(self, input, infer_state:LlamaInferStateInfo, layer_weight:LlamaTransformerLayerWeight)->torch.Tensor:
-        gate_out = torch.mm(input.view(-1, self.embed_dim_), layer_weight.gate_proj)
-        torch.nn.functional.silu(gate_out, inplace=True)
-        up_out = torch.mm(input.view(-1, self.embed_dim_), layer_weight.up_proj)
-        input = None
-        ffn1_out = gate_out * up_out
-        gate_out, up_out = None, None
-        ffn2_out = torch.mm(ffn1_out, layer_weight.down_proj)
-        ffn1_out = None
-        return ffn2_out
-    
     def _copy_kv_to_mem_cache_normal(self, key_buffer, value_buffer, mem_index, mem_manager):
         destindex_copy_kv(key_buffer, mem_index, mem_manager.key_buffer[self.layer_num_])
         destindex_copy_kv(value_buffer, mem_index, mem_manager.value_buffer[self.layer_num_])
         return
     
    
-    def _token_decode_attention_gqa_flashdecoding(self, q, infer_state: LlamaInferStateInfo, layer_weight, out=None):
+    def _token_decode_attention_gqa_flashdecoding(self, q, infer_state: LlamaInferStateInfo, out=None):
         # 对 gqa 模型进行推理优化的代码
         from ..triton_kernel.gqa_flash_decoding import gqa_token_decode_attention_flash_decoding
         cache_k = infer_state.mem_manager.key_buffer[self.layer_num_]
